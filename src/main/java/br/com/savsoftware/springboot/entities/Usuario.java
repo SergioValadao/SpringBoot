8ab43551c4e235f108a5104f020eb6b3d01cde62
SergioValadao <sergiovaladao@gmail.com>
2022-08-31 10:23:44 -0300

    Create table order and relations[C

--- src/main/java/br/com/savsoftware/springboot/entities/Usuario.java
+++ src/main/java/br/com/savsoftware/springboot/entities/Usuario.java
@@ -1,14 +1,19 @@
 package br.com.savsoftware.springboot.entities;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
 
 @Entity
+@Table(name = "tb_usuario")
 public class Usuario implements Serializable {
 	
 	private static final long serialVersionUID = 1L;
@@ -20,6 +25,9 @@ public class Usuario implements Serializable {
 	private String email;
 	private String phone;
 	private String password;
+	
+	@OneToMany(mappedBy = "client")
+	private List<Order> orders = new ArrayList<>();
 	
 	public Usuario() {
 	}
@@ -70,6 +78,10 @@ public class Usuario implements Serializable {
 
 	public void setPassword(String password) {
 		this.password = password;
+	}
+	
+	public List<Order> getOrders() {
+		return orders;
 	}
 	
 	@Override
